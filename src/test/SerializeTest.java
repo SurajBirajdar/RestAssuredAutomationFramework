37f3c20d62a234c4279b019d74555724b529098a
SurajBirajdar <78243934+SurajBirajdar@users.noreply.github.com>
2024-11-01 17:01:14 +0530

    Updated SerializeTest java class

--- src/test/SerializeTest.java
+++ src/test/SerializeTest.java
@@ -3,19 +3,41 @@ package test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
+import test.googlePlaceApiPojo.AddPlaceApi;
+import test.googlePlaceApiPojo.Location;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
-
 public class SerializeTest {
 
     @Test
     public void serializeTest() {
         RestAssured.baseURI = "https://rahulshettyacademy.com";
+        AddPlaceApi addPlace = new AddPlaceApi();
+        addPlace.setAccuracy(50);
+        addPlace.setName("Frontline house");
+        addPlace.setPhone_number("(+91) 983 893 3937");
+        addPlace.setAddress("29, side layout, cohen 09");
+        addPlace.setWebsite("http://google.com");
+        addPlace.setLanguage("French-IN");
+
+        List<String> types = new ArrayList<>();
+        types.add("shoe park");
+        types.add("shop");
+        addPlace.setTypes(types);
+
+        Location location = new Location();
+        location.setLat(-38.383494);
+        location.setLng(33.427362);
+        addPlace.setLocation(location);
         Response response = given()
                 .log()
                 .all()
                 .queryParam("key","qaclick123")
-                .body()
+                .body(addPlace)
                 .when().post("/maps/api/place/add/json")
                 .then()
                 .log()
@@ -26,6 +48,7 @@ public class SerializeTest {
                 .response();
 
         String responseAsString = response.asString();
+        System.out.println(responseAsString);
 
     }
 
